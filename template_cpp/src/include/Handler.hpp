ff0d248b860248e7928b892a38cdb1cc72076fac
amey-kulkarni27 <amey2704@gmail.com>
2023-12-03 14:09:19 +0100

    broadcast and logging decoupled

--- template_cpp/src/include/Handler.hpp
+++ template_cpp/src/include/Handler.hpp
@@ -22,6 +22,9 @@ public:
 	// Constructor named initialise, because we wanted to create a global object
 	Handler(unsigned long id, const char *outputPath, unsigned long num_messages_, std::vector<Parser::Host> hosts) : fub(id, hosts), lg(outputPath), flr((this->fub).getFLSend(), (this->fub).getStubborn(), (this->fub).getPLBroadcast(), (this->fub).getSocket(), id, hosts, this->lg){
 		num_messages = num_messages_;
+
+		std::thread loggingThread(&Handler::logMessages, this);
+		loggingThread.detach();
 	}
 
 	void startExchange(){
@@ -47,11 +50,10 @@ private:
 
 
 
-	std::string createMsgAppendToLogs(unsigned long st, unsigned long en){
+	std::string createMsg(unsigned long st, unsigned long en){
 		std::string payload = "";
 		while(st < en){
 			std::string msg = std::to_string(st);
-			(this->lg).log(msg, false, -1);
 			payload += msg + "_";
 			st++;
 		}
@@ -60,13 +62,24 @@ private:
 
 	void broadcast(){
 		// 1) Create packets containing 8 messages
-		// 2) Log them and send them through the perfect links abstraction
+		// 2) Send them through the perfect links abstraction
 		unsigned long i = 1;
 		while(i <= num_messages){
 			unsigned long end = std::min(i + 8, num_messages + 1);
-			std::string msgToSend = createMsgAppendToLogs(i, end);
+			std::string msgToSend = createMsg(i, end);
 			(this->fub).broadcast(msgToSend);
 			i = end;
+		}
+	}
+
+	void logMessages(){
+		// 1) Create packets containing 8 messages
+		// 2) Log them 
+		unsigned long i = 1;
+		while(i <= num_messages){
+			std::string msg = std::to_string(i);
+			(this->lg).log(msg, false, -1);
+			i++;
 		}
 	}
 
