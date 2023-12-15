e31192a84f2db5c72781638160df6ee4af573066
amey-kulkarni27 <amey2704@gmail.com>
2023-12-15 18:49:55 +0100

    slow logger

--- template_cpp/src/include/Logger.hpp
+++ template_cpp/src/include/Logger.hpp
@@ -2,9 +2,7 @@
 
 #include <iostream>
 #include <string>
-#include <unordered_map>
-#include <pthread.h>
-#include <semaphore.h>
+#include <unordered_set>
 
 #include "parser.hpp"
 #include "Helper.hpp"
@@ -13,48 +11,32 @@ class Logger{
 
 public:
 
-	Logger(const char *oPath): filePath(oPath), outputFile(filePath){
+	Logger(const char *oPath, unsigned long n_): filePath(oPath), outputFile(filePath), n(n_){
 		createFile();
-		pthread_mutex_init(&logsLock, NULL);
-		sem_init(&spotsLeft, 0, MAX_QUEUE_SIZE);
-		sem_init(&spotsFilled, 0, 0);
-
-		std::thread consumerThread(&Logger::flush, this);
-		consumerThread.detach();
+		logs.resize(n + 1);
+		has.resize(n + 1, false);
+		ptr = 1;
 	}
 
-	~Logger(){
-		sem_destroy(&spotsLeft);
-		sem_destroy(&spotsFilled);
-		pthread_mutex_destroy(&logsLock);
+
+	void logAndFlush(unsigned long pos, std::unordered_set<unsigned long> nums){
+		logs[pos] = nums;
+		while(ptr <= n and has[ptr]){
+			writeNewLine(nums);
+			logs[ptr].clear();
+			ptr++;
+		}
 	}
 
-	void log(std::string msg, bool delivery, unsigned long senderId){
-		std::string logMsg = "";
-		if(delivery)
-			logMsg = "d " + std::to_string(senderId) + " " + msg;
-		else
-			logMsg = "b " + msg;
-		sem_wait(&spotsLeft);
-		pthread_mutex_lock(&logsLock);
-		logs.push(logMsg);
-		pthread_mutex_unlock(&logsLock);
-		sem_post(&spotsFilled);
-	}
-
-	void stopAll(){
-		flushing = false;
-		emptyLogs();
-	}
 
 
 private:
-	const int MAX_QUEUE_SIZE = 100000;
-	std::queue<std::string> logs;
-	pthread_mutex_t logsLock;
-	sem_t spotsLeft, spotsFilled;
+	std::vector<std::unordered_set<unsigned long>> logs;
+	std::vector<bool> has;
+	int ptr;
 	std::string filePath;
 	std::ofstream outputFile;
+	unsigned long n;
 	bool flushing = true;
 
 	void createFile(){
@@ -64,29 +46,10 @@ private:
 		}
 	}
 
-	void flush(){
-		while(flushing){
-			sem_wait(&spotsFilled);
-			pthread_mutex_lock(&logsLock);
-			std::string msg = logs.front();
-			outputFile<<msg<<std::endl;
-			logs.pop();
-			pthread_mutex_unlock(&logsLock);
-			sem_post(&spotsLeft);
-		}
-	}
-
-	// Make this the last operation
-	void emptyLogs(){
-		while(!logs.empty()){
-			sem_wait(&spotsFilled);
-			pthread_mutex_lock(&logsLock);
-			std::string msg = logs.front();
-			outputFile<<msg<<std::endl;
-			logs.pop();
-			pthread_mutex_unlock(&logsLock);
-			sem_post(&spotsLeft);
-		}
+	void writeNewLine(std::unordered_set<unsigned long> nums){
+		for(const unsigned long &num: nums)
+			outputFile<<num<<" ";
+		outputFile<<std::endl;
 	}
 
 };
